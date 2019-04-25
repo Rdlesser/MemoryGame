149be7862e28b70ec42d528cc9c5d7972bc5a601
Robert Lesser <robertlesser@infiapps.com>
2019-04-25 13:51:24 +0300

    Started the graphics part

--- MemoryGame/Assets/MemoryGame/Scripts/GameRunnerGraphics.cs
+++ MemoryGame/Assets/MemoryGame/Scripts/GameRunnerGraphics.cs
@@ -2,15 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameRunnerGraphics: MonoBehaviour {
+public class GameRunnerGraphics: MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	public void InitializedEnvironment(eCard[,] board)
+	{
 		
 	}
 }
